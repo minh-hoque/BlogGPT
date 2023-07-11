e19c7f2895e6c86e04f7c8ab17c639851dc24c0a
Minhajul Hoque <minh.hoque@clarifai.com>
2023-07-11 00:22:13 -0400

    refactored main app

--- bloggpt/main.py
+++ bloggpt/main.py
@@ -1,44 +1,89 @@
 import streamlit as st
-
 from bloggpt import main
 
-# Initialize state variables
-if "topic" not in st.session_state:
-    st.session_state.topic = None
-if "outline" not in st.session_state:
-    st.session_state.outline = None
 
-with st.form("my_form"):
-    topic_str = st.text_input("Topic:")
-    topic_str = "Topic: " + topic_str
+# Function to initialize session state
+def init_session_state():
+    if "topic" not in st.session_state:
+        st.session_state.topic = None
+    if "outline" not in st.session_state:
+        st.session_state.outline = []
+    if "num_sections" not in st.session_state:
+        st.session_state.num_sections = 1
 
-    default_text = """# Header 1
+
+# Function to get default text
+def get_default_text():
+    return """# Header 1
 Description of this blog section. Describe important aspects of this blog section.
 Search for: (keywords)
-    
+
 # Header 2
 Description of this blog section. Describe important aspects of this blog section.
 Search for: (keywords)
-    
+
 # Header 3
 Description of this blog section. Describe important aspects of this blog section.
 Search for: (keywords)
     """
 
-    blog_outline = st.text_area(
-        "Define the blog outline", value=default_text, height=400
-    )
 
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Generate Blog")
-    if submitted:
-        # Store in state variables
-        st.session_state.topic = topic_str
-        st.session_state.outline = blog_outline
+# Function to create a form
+def create_form():
+    with st.form("my_form"):
+        topic_str = st.text_input("Topic:")
+        topic_str = "Topic: " + topic_str
 
-if st.session_state.topic and st.session_state.outline:
-    with st.expander("Blog Outline"):
-        st.write(f"# {st.session_state.topic}")
-        st.write(st.session_state.outline)
+        blog_outline = []
+        for i in range(st.session_state.num_sections):
+            outline_section = st.text_area(
+                f"Define the blog section {i + 1}",
+                value=f"# Header {i + 1}\nDescription of this blog section. Describe important aspects of this blog section.\nSearch for: (keywords)",
+                height=200,
+            )
+            blog_outline.append(outline_section)
 
-    main(topic_str=topic_str, blog_outline=blog_outline)
+        # Add button to increase the number of blog sections
+        cols = st.columns(3)
+        with cols[0]:
+            if st.form_submit_button("Add new blog section"):
+                st.session_state.num_sections += 1
+                st.experimental_rerun()
+        with cols[1]:
+            if st.form_submit_button("Remove last blog section"):
+                st.session_state.num_sections -= 1
+                st.experimental_rerun()
+        with cols[2]:
+            # Every form must have a submit button.
+            submitted = st.form_submit_button("Generate Blog")
+
+        if submitted:
+            # Store in state variables
+            st.session_state.topic = topic_str
+            st.session_state.outline = "\n".join(blog_outline)
+
+
+# Function to display blog outline
+def display_blog_outline():
+    if st.session_state.topic and st.session_state.outline:
+        with st.expander("Blog Outline"):
+            st.write(f"# {st.session_state.topic}")
+            st.write(st.session_state.outline)
+
+        try:
+            main(
+                topic_str=st.session_state.topic, blog_outline=st.session_state.outline
+            )
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+
+
+# Initialize session state
+init_session_state()
+
+
+# Create form
+create_form()
+
+# Display blog outline
+display_blog_outline()
