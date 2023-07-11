6594a8f42fbdb474a369223c7ea673c28e5fb914
Minhajul Hoque <minh.hoque@clarifai.com>
2023-07-11 16:00:49 -0400

    Added helper functions

--- bloggpt/main.py
+++ bloggpt/main.py
@@ -12,20 +12,10 @@ def init_session_state():
         st.session_state.num_sections = 1
 
 
-# Function to get default text
-def get_default_text():
-    return """# Header 1
+def get_default_text(section_num):
+    return f"""# Header {section_num}
 Description of this blog section. Describe important aspects of this blog section.
-Search for: (keywords)
-
-# Header 2
-Description of this blog section. Describe important aspects of this blog section.
-Search for: (keywords)
-
-# Header 3
-Description of this blog section. Describe important aspects of this blog section.
-Search for: (keywords)
-    """
+Search for: (keywords)"""
 
 
 # Function to create a form
@@ -38,7 +28,7 @@ def create_form():
         for i in range(st.session_state.num_sections):
             outline_section = st.text_area(
                 f"Define the blog section {i + 1}",
-                value=f"# Header {i + 1}\nDescription of this blog section. Describe important aspects of this blog section.\nSearch for: (keywords)",
+                value=get_default_text(i + 1),
                 height=200,
             )
             blog_outline.append(outline_section)
@@ -60,7 +50,7 @@ def create_form():
         if submitted:
             # Store in state variables
             st.session_state.topic = topic_str
-            st.session_state.outline = "\n".join(blog_outline)
+            st.session_state.outline = "\n\n".join(blog_outline)
 
 
 # Function to display blog outline
